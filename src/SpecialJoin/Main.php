ee015d0e260e8106820969afd4a1fea939f9cdcd
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-08-13 22:06:24 -0400

    Update Main.php

--- src/SpecialJoin/Main.php
+++ src/SpecialJoin/Main.php
@@ -21,8 +21,7 @@ public function onJoin(PlayerJoinEvent $event){
 $p = $event->getPlayer();
 $pname = $p->getName();
 if($pname == "ChaoticSynergy" or $pname == "xShootingStars" or $pname == "XFuryTactics" or $pname == "xFuryTactics" or $pname == "PocketGaming"){
-$event->setMessage("");
-$this->getServer()->broadcastMessage(TextFormat::GOLD."" . TextFormat::BOLD . $pname . " " . TextFormat::RESET . TextFormat::GREEN . "Joined The Game!");
+$event->setMessage(TextFormat::GOLD."" . TextFormat::BOLD . $pname . " " . TextFormat::RESET . TextFormat::GREEN . "Joined The Game!");
 }else{
 $event->setMessage("");
 }
